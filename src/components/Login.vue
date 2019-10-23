78ae250497d432d433bc94de9fa74c7d18224ff2
Ann <wanfangliao@icloud.com>
2019-10-23 22:07:03 +0800

    login finished

---
+++ src/components/Login.vue
@@ -0,0 +1,101 @@
+<template>
+<div class="login">
+  <el-form status-icon :rules="rules" ref="form" :model="form" label-width="80px">
+    <img src="../assets/01.jpg" class="logo" alt="">
+  <el-form-item label="用户名" prop="username">
+    <el-input v-model="form.username" placeholder="please enter username or email"></el-input>
+  </el-form-item>
+   <el-form-item label="密码" prop="password">
+    <el-input type="password" v-model="form.password" placeholder="please enter password"></el-input>
+  </el-form-item>
+   <el-form-item >
+    <el-button @click="login" type="primary" class="submit">登录</el-button>
+    <el-button @click="reset" class="reset">重置</el-button>
+  </el-form-item>
+</el-form>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      form: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: 'username is required', trigger: ['change', 'blur'] },
+          { min: 3, max: 12, message: 'length should be between 3-12', trigger: ['change', 'blur'] }
+        ],
+        password: [
+          { required: true, message: 'password is needed', trigger: ['change', 'blur'] },
+          { min: 3, max: 12, message: 'password is between 3-12', trigger: ['change', 'blur'] }
+        ]
+      }
+    }
+  },
+  methods: {
+    reset () {
+      this.$refs.form.resetFields()
+    },
+    login () {
+      this.$refs.form.validate(isValid => {
+        if (!isValid) return
+        axios.post('http://localhost:8888/api/private/v1/login', this.form)
+          .then(res => {
+            const { meta } = res.data
+            if (meta.status === 200) {
+              this.$message.success('login successfully')
+              this.$router.push('/index')
+            } else {
+              this.$message.error('failure in login')
+            }
+          })
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less">
+*{
+  padding: 0;
+  margin: 0;
+}
+html, body, #app {
+  width: 100%;
+  height: 100%;
+  .login{
+     width: 100%;
+     height: 100%;
+    background-color: #2d434c;
+    overflow: hidden;
+    .el-form{
+      width: 400px;
+      padding: 20px;
+      padding-top: 70px;
+      border-radius: 20px;
+      background-color: #fff;
+      margin: 0 auto;
+      margin-top: 100px;
+      position: relative;
+      .reset{
+        margin-left: 70px;
+      }
+      .logo{
+        width: 120px;
+        border-radius: 50%;
+        border: 5px solid #fff;
+        position: absolute;
+        left:  50%;
+        transform: translateX(-50%);
+        top: -70px;
+      }
+    }
+  }
+}
+
+</style>
